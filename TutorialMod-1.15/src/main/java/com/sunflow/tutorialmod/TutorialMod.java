0449b87258aa1a08be7035bd0600aca1c1c3dd35
SunFlow <sunflow.riven@gmai.com>
2020-06-29 13:29:24 +0200

    fixed pack.mcmeta comma on wrong line

--- TutorialMod-1.15/src/main/java/com/sunflow/tutorialmod/TutorialMod.java
+++ TutorialMod-1.15/src/main/java/com/sunflow/tutorialmod/TutorialMod.java
@@ -21,8 +21,8 @@ import net.minecraftforge.versions.mcp.MCPVersion;
 public class TutorialMod {
 	public static final String MODID = "tutorialmod";
 	public static final String NAME = "Tutorial Mod";
-	public static final String VERSION = "1.2.0";
-	public static final String ACCEPTED_VERSION = "[1.15.2,)";
+	public static final String VERSION = "1.3.0";
+	public static final String ACCEPTED_VERSION = "[1.15.2]";
 
 //	public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
 	public static CommonProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
