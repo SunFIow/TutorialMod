2729fe9d09d5c0d313274d567978400e4eefb986
SunFlow <sunflow.riven@gmai.com>
2020-06-29 13:16:16 +0200

    Revert "Created 1.16 TutorialMod ("Runnable")"
    This reverts commit c0ad25ef8d1d0cc103e8f6cf9056329ed49a3dcb.

--- TutorialMod-1.15/src/main/java/com/sunflow/tutorialmod/TutorialMod.java
+++ TutorialMod-1.15/src/main/java/com/sunflow/tutorialmod/TutorialMod.java
@@ -22,7 +22,7 @@ public class TutorialMod {
 	public static final String MODID = "tutorialmod";
 	public static final String NAME = "Tutorial Mod";
 	public static final String VERSION = "1.2.0";
-	public static final String ACCEPTED_VERSION = "[1.15.2,)";
+	public static final String ACCEPTED_VERSION = "[1.15.1,)";
 
 //	public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
 	public static CommonProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
--- TutorialMod-1.16/src/main/java/com/sunflow/tutorialmod/util/interfaces/IEnergyItem.java
+++
@@ -1,7 +0,0 @@
-package com.sunflow.tutorialmod.util.interfaces;
-
-import com.sunflow.tutorialmod.util.CustomEnergyStorage;
-
-public interface IEnergyItem {
-	CustomEnergyStorage createEnergy();
-}
